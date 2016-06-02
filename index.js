6cf3494251d8a909bf039cc45c2ec381a1bca0da
frozinoer <fabrice.rozinoer@gmail.com>
2016-06-02 09:15:02 +0200

    First version of working chat

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-var express = require('express');
-
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var apiai = require('apiai');
+var apiaiClient = apiai("82e98d777ee945bc912643cd073a9a20");
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World !');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.use(function(req, res, next){
-    res.setHeader('Content-Type', 'text/plain');
-    res.send(404, 'Page introuvable !');
+io.on('connection', function(socket){
+  	socket.on('chat message', function(msg){
+  	var request = apiaiClient.textRequest(msg);
+  	request.end();
+    io.emit('chat message', 'MOI > ' + msg);
+	request.on('response', function(response) {
+	    console.log(response);
+    	io.emit('chat message', 'ROBOT > ' + response.result.fulfillment.speech);	    
+	});
+
+ 
+	request.on('error', function(error) {
+	    console.log(error);
+	});
+
+
+  });
 });
 
-app.listen(app.get('port'));
-
+http.listen(app.get('port'), function(){
+  console.log('listening on *:' + app.get('port'));
+});
