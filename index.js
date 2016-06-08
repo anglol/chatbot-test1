a2b24538c25bf26d738f845120d4aad0f164a7bf
frozinoer <fabrice.rozinoer@gmail.com>
2016-06-08 23:01:21 +0200

    Major changes
    - full data is sent to the client-side
    - a data file containing automatically fills "info.*" speech fulfillment
    - chat messages can display <br /> (when a message contains a carriage
    return)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var apiai = require('apiai');
 var apiaiClient = apiai("82e98d777ee945bc912643cd073a9a20");
+var jsonfile = require('jsonfile');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
@@ -12,42 +13,24 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/index.css', function(req, res){
+/*app.get('/index.css', function(req, res){
   res.sendFile(__dirname + '/index.css');
-});
+});*/
 
+var jsonfile = require('jsonfile')
 
-function getRandomIntBetween(low, high) {
-    return Math.floor(Math.random() * (high - low + 1) + low);
-}
+var file = './data.json'
 
-function getRandomExcuse() {
+var infoPrefix = "info.";
 
-	var excuses = [
-
-		'I am sorry. I haven\'t yet learned how to answer this question :)',
-		'Please try to tell me that another way.',
-		'Sorry, I don\'t know what that means.',
-		'Mmmmh I don\'t understand, sorry.',
-		'I wish I understand, but I don\'t.',
-		'I feel stupid now.',
-		'I don\'t understand yet, I just born !',
-		'I have no idea what you are talking about :D',
-		'Could you try to write it another way ?',
-		'This is awkward, I have no idea about it.',
-		'Sorry, I\'m not smarter than an octopus.'
-
-	];
-	var index = getRandomIntBetween(0, excuses.length - 1);
-
-	return excuses[index];
-}
-
+var helloData = jsonfile.readFileSync(file);
+ 
 io.on('connection', function(socket){
   	socket.on('chat message', function(msg){
   	
   		if (msg) {
-		  	var request = apiaiClient.textRequest(msg.message, {sessionId: socket.id});
+
+		  	var request = apiaiClient.textRequest(msg.message, {sessionId: socket.id, location: msg.location});
 		  	request.end();
 
 		  	var payload = {
@@ -58,12 +41,24 @@ io.on('connection', function(socket){
 		    socket.emit('chat message', payload);
 			request.on('response', function(response) {
 
-				console.log(response);
+				var result = response.result;
 
 			  	var payload = {
 			  		from: "api.ai",
-			  		content: (response.result.fulfillment.speech || getRandomExcuse())
+			  		content: result
 			  	};				
+
+				if (result && result.action.startsWith(infoPrefix)) {
+
+					var id = result.action.substr(infoPrefix.length);
+					console.log(helloData[id])					
+					var item = helloData[id];
+
+					var speech = item.response;
+					payload.content.fulfillment.speech = speech;
+										
+				}
+
 		    	socket.emit('chat message', payload);
 			});
   		}
--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,30 @@ var $messages = $('.messages-content'),
     d, h, m,
     i = 0;
 
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+var position;
+
+
+function geolocSuccess(pos) {
+  position = pos;
+
+  console.log('Your current position is:');
+  console.log('Latitude : ' + position.coords.latitude);
+  console.log('Longitude: ' + position.coords.longitude);
+  console.log('More or less ' + position.coords.accuracy + ' meters.');
+};
+
+function geolocError(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+
+
 $(window).load(function() {
 
   $messages.mCustomScrollbar();
@@ -11,6 +35,10 @@ $(window).load(function() {
   }, 100);
 
 
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError, options);
+  }
+
   socket = io();
 
   $('.message-submit').click(function() {
@@ -19,9 +47,12 @@ $(window).load(function() {
       return false;
     }
 
+
+
     var payload = {
-      message: $('.message-input').val(),
+      message: $('.message-input').val()
     }
+
 
     socket.emit('chat message', payload);
     $('.message-input').val(null);
@@ -32,16 +63,34 @@ $(window).load(function() {
 
   socket.on('chat message', function(response){
 
-    if (response.from === "self") {
-      $('<div class="message message-personal">' + response.content + '</div>').appendTo($('.mCSB_container')).addClass('new');
-      $('<div class="message loading new"><figure class="avatar"><img src="images/apiai.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));          
-    } else {
-      $('.message.loading').remove();
-      $('<div class="message new"><figure class="avatar"><img src="images/apiai.png" /></figure>' + response.content + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    console.log(response);
+    $('.message.loading').remove();
+
+    var text;
+
+
+    switch(response.from) {
+
+
+
+      case "self":
+        text = response.content.replace(/\n/g, "<br />");
+        $('<div class="message message-personal">' + text + '</div>').appendTo($('.mCSB_container')).addClass('new');
+        $('<div class="message loading new"><figure class="avatar"><img src="images/apiai.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));                  
+        break;
+
+      case "api.ai":
+
+        switch(response.content.action) {
+          default:
+            text = (response.content.fulfillment.speech.replace(/\n/g, "<br />") || getRandomText());
+            $('<div class="message new"><figure class="avatar"><img src="images/apiai.png" /></figure>' + text + '</div>').appendTo($('.mCSB_container')).addClass('new');        
+            break;
+        }
     }
+
     updateScrollbar();
 
-//        $('#messages').append($('<li>').text(msg));
   });
 
 
@@ -62,28 +111,30 @@ function setDate(){
   }
 }
 
+
+
 function insertMessage() {
   msg = $('.message-input').val();
   if ($.trim(msg) == '') {
     return false;
   }
 
+
   var payload = {
     message: $('.message-input').val(),
   }
 
+  if (position && position.coords) {
+      payload.location = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      }
+  }
+
+
   socket.emit('chat message', payload);
   $('.message-input').val('');
   return false;                      
-
-
-/*  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
-  setDate();
-  $('.message-input').val(null);
-  updateScrollbar();
-  setTimeout(function() {
-    fakeMessage();
-  }, 1000 + (Math.random() * 20) * 100);*/
 }
 
 $('.message-submit').click(function() {
@@ -95,39 +146,29 @@ $(window).on('keydown', function(e) {
     insertMessage();
     return false;
   }
-})
+});
 
-var Fake = [
-  'Hi there, I\'m Fabio and you?',
-  'Nice to meet you',
-  'How are you?',
-  'Not too bad, thanks',
-  'What do you do?',
-  'That\'s awesome',
-  'Codepen is a nice place to stay',
-  'I think you\'re a nice person',
-  'Why do you think that?',
-  'Can you explain?',
-  'Anyway I\'ve gotta go now',
-  'It was a pleasure chat with you',
-  'Time to make a new codepen',
-  'Bye',
-  ':)'
-]
+function getRandomIntBetween(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
 
-function fakeMessage() {
-  if ($('.message-input').val() != '') {
-    return false;
-  }
-  $('<div class="message loading new"><figure class="avatar"><img src="images/apiai.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
-  updateScrollbar();
 
-  setTimeout(function() {
-    $('.message.loading').remove();
-    $('<div class="message new"><figure class="avatar"><img src="images/apiai.png" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
-    setDate();
-    updateScrollbar();
-    i++;
-  }, 1000 + (Math.random() * 20) * 100);
+function getRandomText() {
 
+  var excuses = [
+
+    'I am sorry. I haven\'t yet learned how to answer this question :)',
+    'Please try to tell me that another way.',
+    'Sorry, I don\'t know what that means.',
+    'Mmmmh I don\'t understand, sorry.',
+    'I wish I understand, but I don\'t.',
+    'I feel stupid now.',
+    'I don\'t understand yet, I just born !',
+    'I have no idea what you are talking about :D',
+    'Could you try to write it another way ?',
+    'This is awkward, I have no idea about it.',
+    'Sorry, I\'m not smarter than an octopus.'
+
+  ];
+  return excuses[getRandomIntBetween(0, excuses.length - 1)];
 }
